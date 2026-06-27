78f54febc5d3c3aea3d77668da73004d3b935a0d
tobalpintoo <cpinto.py@gmail.com>
2026-06-27 19:47:48 -0400

    Configuracion y estructuracion de carpeta server(backend)

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const PORT = 3000;
+
+const app = express();
+
+app.listen(PORT, console.log(`Server is listening on port 3000`))
+
+app.get("/", (req, res) => {
+    res.send("Hello world!")
+})
