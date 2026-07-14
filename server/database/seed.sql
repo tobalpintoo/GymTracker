deb9bccdf7369a220e1087a795e41f6b0a027fa7
tobalpintoo <cpinto.py@gmail.com>
2026-07-13 22:49:38 -0400

    chore(db): creacion de estructura inicial y scrips de insercion

---
+++ server/database/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO exercises (name, muscle_group, description) VALUES
+('Press de Banca', 'Pecho', 'Empuje horizontal con barra'),
+('Sentadilla con Barra', 'Piernas', 'Sentadilla profunda con carga'),
+('Dominadas', 'Espalda', 'Tracción vertical en barra fija'),
+('Press Militar', 'Hombros', 'Empuje vertical sobre la cabeza'),
+('Curl de Bíceps', 'Bíceps', 'Flexión de codo con mancuernas'); 
